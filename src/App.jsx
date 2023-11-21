0a6ebd2cd353c5215d1eb7fbe0d3eec787bd41ac
TiavinaMalalaniaina <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2023-11-21 11:42:16 +0300

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,99 @@
+import { useEffect, useState } from "react";
+import PlayerList from "./views/pages/PlayerList";
 
 function App() {
+
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      try {
+        // const response = await axios.get('https://api.example.com/players');
+        const response = {
+          data: 
+          [
+            {
+              nom: "Tiavina",
+              equipe: "EAGLE",
+              stat: {
+                match: 5,
+                matchJoue: 10,
+                pointParMatch: 10,
+                rebondParMatch: 20,
+                passeDecisifParMatch: 20,
+                minuteParMatch: 10,
+                efficacite: 20,
+                fieldGoal: 10,
+                threePoint: 2,
+                lancerFront: 10
+              }
+            },
+            {
+              nom: "Tiavina",
+              equipe: "EAGLE",
+              stat: {
+                match: 5,
+                matchJoue: 10,
+                pointParMatch: 10,
+                rebondParMatch: 20,
+                passeDecisifParMatch: 20,
+                minuteParMatch: 10,
+                efficacite: 20,
+                fieldGoal: 10,
+                threePoint: 2,
+                lancerFront: 10
+              }
+            },
+            {
+              nom: "Tiavina",
+              equipe: "EAGLE",
+              stat: {
+                match: 5,
+                matchJoue: 10,
+                pointParMatch: 10,
+                rebondParMatch: 20,
+                passeDecisifParMatch: 20,
+                minuteParMatch: 10,
+                efficacite: 20,
+                fieldGoal: 10,
+                threePoint: 2,
+                lancerFront: 10
+              }
+            },
+            {
+              nom: "Tiavina",
+              equipe: "EAGLE",
+              stat: {
+                match: 5,
+                matchJoue: 10,
+                pointParMatch: 10,
+                rebondParMatch: 20,
+                passeDecisifParMatch: 20,
+                minuteParMatch: 10,
+                efficacite: 20,
+                fieldGoal: 10,
+                threePoint: 2,
+                lancerFront: 10
+              }
+            },
+          ],
+          exception: ""
+        }
+        setPlayers(response.data);
+      } catch (error) {
+        // console.error('Erreur lors du chargement des joueurs :', error);
+      }
+    };
+
+    fetchPlayers();
+  }, []); 
+
+
+
   return (
     <div className="App" style={({ height: "100vh" }, { display: "flex" })}>
         <div className="container" style={{ padding: "50px" }}>
           <div className="row">
-            <p>Test</p>
+            <PlayerList players={players}/>
           </div>
         </div>
     </div>
---
+++ src/views/pages/PlayerList.jsx
@@ -0,0 +1,49 @@
+import { Table } from 'react-bootstrap';
+
+const PlayerList = ({players}) => {
+  
+  return (
+    <div className="container mt-5">
+    <h2>Statistiques des joueurs NBA</h2>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Équipe</th>
+          <th>M</th>
+          <th>MJ</th>
+          <th>PPM</th>
+          <th>RPM</th>
+          <th>PDPM</th>
+          <th>MPM</th>
+          <th>EFF</th>
+          <th>FG%</th>
+          <th>3P%</th>
+          <th>%LF</th>
+        </tr>
+      </thead>
+      <tbody>
+        {players.map((player, index) => (
+          <tr key={index}>
+            <td>{ index + 1 }</td>
+            <td>{player.nom}</td>
+            <td>{player.equipe}</td>
+            <td>{player.stat.match}</td>
+            <td>{player.stat.matchJoue}</td>
+            <td>{player.stat.pointParMatch}</td>
+            <td>{player.stat.rebondParMatch}</td>
+            <td>{player.stat.passeDecisifParMatch}</td>
+            <td>{player.stat.minuteParMatch}</td>
+            <td>{player.stat.efficacite}</td>
+            <td>{player.stat.fieldGoal}</td>
+            <td>{player.stat.threePoint}</td>
+            <td>{player.stat.lancerFront}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  </div>
+  );
+};
+
+export default PlayerList;
